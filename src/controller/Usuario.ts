a25c71d92b08f96d30856a683a641a6838ecb730
Diego Marinho <marinhop.diego@gmail.com>
2018-10-09 13:58:26 -0300

    feat: added get function to usuario controller

--- src/controller/Usuario.ts
+++ src/controller/Usuario.ts
@@ -1,6 +1,16 @@
 import { Request, Response, NextFunction } from 'express';
 import UsuarioModel from '../model/Usuario';
 
-class UsuarioController { }
+class UsuarioController {
+    async get(req: Request, res: Response, next: NextFunction): Promise<void> {
+        try {
+            let item = await UsuarioModel.get();
+            res.json(item);
+        } catch(error) {
+            res.status(500);
+            next(error);
+        }
+    }
+}
 
 export default UsuarioController;
