839ff29da412e2b8510b86c9feb6561a0b000f27
Diego Marinho <marinhop.diego@gmail.com>
2018-10-09 14:30:40 -0300

    feat: added get function to livro controller

--- src/controller/Livro.ts
+++ src/controller/Livro.ts
@@ -1,6 +1,16 @@
 import { Request, Response, NextFunction } from 'express';
 import LivroModel from '../model/Livro';
 
-class LivroController { }
+class LivroController {
+    async get(req: Request, res: Response, next: NextFunction): Promise<void> {
+        try {
+            let item = await LivroModel.get();
+            res.json(item);
+        } catch(error) {
+            res.status(500);
+            next(error);
+        }
+    }
+}
 
 export default LivroController;
