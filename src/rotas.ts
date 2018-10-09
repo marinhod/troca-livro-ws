5de0ebf4ad56e002cd401388168bd98429725951
Diego Marinho <marinhop.diego@gmail.com>
2018-10-09 14:33:24 -0300

    feat: added usuario get route

--- src/rotas.ts
+++ src/rotas.ts
@@ -6,7 +6,13 @@ const ROUTER = express.Router();
 const USUARIO_CONTROLLER = new UsuarioController();
 const USUARIO_URL = 'usuario';
 
+const LIVRO_CONTROLLER = new UsuarioController();
+const LIVRO_URL = 'livro';
+
 ROUTER
 .get(`/${USUARIO_URL}/:id`, USUARIO_CONTROLLER.get);
+
+ROUTER
+.get(`/${LIVRO_URL}/:id`, LIVRO_CONTROLLER.get);
 
 export default ROUTER;
